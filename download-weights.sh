ff6b77089f4f51a6169fa589ecf301c0d95ecbb7
gaojie <2482-gaojie@users.noreply.git.sysop.bigo.sg>
2025-06-05 20:04:03 +0800

    fix: fix init env

--- download-weights.sh
+++ download-weights.sh
@@ -6,7 +6,11 @@ rm checkpoints.zip
 wget --no-check-certificate "https://huggingface.co/MonsterMMORPG/tools/resolve/main/antelopev2.zip" -O antelope.zip
 mkdir -p insightface_func/models
 unzip ./antelope.zip -d ./insightface_func/models/
+mv ./insightface_func/models/antelopev2 ./insightface_func/models/antelope
 rm antelope.zip
+
+mkdir -p ./parsing_model/checkpoint
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=154JgKpzCPW82qINcVieuPH3fZ2e0P812" -O ./parsing_model/checkpoint/79999_iter.pth
 
 wget https://github.com/neuralchen/SimSwap/releases/download/512_beta/512.zip
 unzip ./512.zip -d ./checkpoints
